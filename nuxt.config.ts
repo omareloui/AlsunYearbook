fbbc79b855cde4c8c1c2663207f0601c7d9f4850
Omar Hussein <omarelwy@hotmail.com>
2022-01-24 10:54:49 +0200

    ✨ Add style files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,25 @@
 import { defineNuxtConfig } from "nuxt3";
 
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  meta: {
+    link: [
+      // { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "preconnect", href: "https://fonts.googleapis.com" },
+      {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossorigin: true,
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Poppins&display=swap",
+      },
+    ],
+  },
+  css: [
+    "~/assets/styles/core/index.scss",
+    "~/assets/styles/variables/index.scss",
+    "~/assets/styles/base/index.scss",
+    "~/assets/styles/utilities/index.scss",
+  ],
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,3 @@
-<script setup lang="ts">
-</script>
-
-<template></template>
+<template>hi</template>
 
 <style scoped lang="sass"></style>
