1ac50e4c07191772a1f69d0e2860c51213e2e4cc
Omar Hussein <omarelwy@hotmail.com>
2022-02-25 20:44:13 +0200

    🔥 Remove custom routes

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,13 +1,10 @@
 import express from "express";
 
 import { connect as connectDB } from "~~/server/db";
-import { router } from "~~/server/routes";
 
 const app = express();
 
 app.use(express.json());
-
-app.use("/api", router);
 
 connectDB();
 
--- server/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { UserController } from "../controllers/user.controller";
-
-const router = Router();
-
-router.get("/users", UserController.getAll);
-
-export { router };
