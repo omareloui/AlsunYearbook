2626ed9515e9012742c5547aa200e53a39c74fac
Omar Hussein <omarelwy@hotmail.com>
2022-02-27 16:43:55 +0200

    ✨ Add v-model for search input

--- components/Input/Search.vue
+++ components/Input/Search.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{ modelValue: string }>();
+
+const emit = defineEmits(["update:modelValue"]);
+const content = useModelWrapper(props, emit);
+</script>
+
 <template>
-  <InputBase type="text" placeholder="Type to search..." is-large />
+  <InputBase
+    type="text"
+    placeholder="Type to search..."
+    is-large
+    v-model="content"
+  />
 </template>
