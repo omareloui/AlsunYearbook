805e773a38907d821c64b49ad623b6870446f460
Omar Hussein <omarelwy@hotmail.com>
2022-03-08 06:19:16 +0200

    🎨 Always go to homepage on sign pages

--- middleware/already-authenticated.global.ts
+++ middleware/already-authenticated.global.ts
@@ -1,10 +1,6 @@
-import { useAuthStore } from "~~/store/useAuth";
-
 export default defineNuxtRouteMiddleware(to => {
-  const authStore = useAuthStore();
-
   const routesRegExpToGuard = /^\/sign(in|up)/i;
   const isRouteToGuard = !!to.fullPath.match(routesRegExpToGuard);
 
-  if (isRouteToGuard && authStore.isSigned) return navigateTo("/");
+  if (isRouteToGuard) return navigateTo("/");
 });
