050a487baa52123d4bbecd52010883a8a98a2057
Omar Hussein <omarelwy@hotmail.com>
2022-03-08 06:22:04 +0200

    🐛 Bring back redirecting from sign pages only on already signed

--- middleware/already-authenticated.global.ts
+++ middleware/already-authenticated.global.ts
@@ -1,6 +1,10 @@
-export default defineNuxtRouteMiddleware(to => {
+import { useAuthStore } from "~~/store/useAuth";
+
+export default defineNuxtRouteMiddleware((_from, to) => {
+  const authStore = useAuthStore();
+
   const routesRegExpToGuard = /^\/sign(in|up)/i;
   const isRouteToGuard = !!to.fullPath.match(routesRegExpToGuard);
 
-  if (isRouteToGuard) return navigateTo("/");
+  if (isRouteToGuard && authStore.isSigned) return navigateTo("/");
 });
