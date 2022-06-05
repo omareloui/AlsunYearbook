54b11a338924ac87252f44d7e4e9bcefcc83bede
Omar Hussein <omarelwy@hotmail.com>
2022-06-05 18:53:42 +0200

    🐛 Update parse error to work with new nuxt updates

--- composables/useParseError.ts
+++ composables/useParseError.ts
@@ -4,5 +4,5 @@ export function useParseError(e: Record<string, any>) {
     return matchResult ? matchResult[1] : message;
   }
 
-  return getInsideH1(e.value.data.description || e.value.data).trim();
+  return getInsideH1(e.value.data?.message || e.value.data).trim();
 }
