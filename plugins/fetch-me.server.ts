257aa132166c77fd54b0e72a98ed3fd02dc299f7
Omar Hussein <omarelwy@hotmail.com>
2022-06-10 05:37:34 +0200

    🔥 Remove fetch-me.server.ts

--- plugins/fetch-me.server.ts
+++
@@ -1,9 +0,0 @@
-import { useAuthStore } from "~~/store/useAuth";
-
-export default defineNuxtPlugin(async () => {
-  const authStore = useAuthStore();
-
-  const { user } = await $fetch("/api/me", { headers: useAuthHeaders()() });
-
-  authStore.setUser(user);
-});
