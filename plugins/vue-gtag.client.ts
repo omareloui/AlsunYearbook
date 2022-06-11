d995710d44dda68827033d1c5b2ca0c04e7e379a
Omar Hussein <omarelwy@hotmail.com>
2022-06-11 23:04:28 +0200

    🔨 Apply GTag only on production

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -1,11 +1,12 @@
 import VueGtag from "vue-gtag-next";
 
 export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.vueApp.use(VueGtag, {
-    property: {
-      id: "G-4FW3ZNP50P",
-    },
-  });
+  if (useRuntimeConfig().public.isProd)
+    nuxtApp.vueApp.use(VueGtag, {
+      property: {
+        id: "G-4FW3ZNP50P",
+      },
+    });
 
   return {
     provide: {
