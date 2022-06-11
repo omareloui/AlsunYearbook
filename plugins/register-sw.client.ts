bdf7a6dae0bd8b38aee612cd90eb77299df8213a
Omar Hussein <omarelwy@hotmail.com>
2022-06-11 22:49:15 +0200

    🎨 Remove all sw registrations on developement

--- plugins/register-sw.client.ts
+++ plugins/register-sw.client.ts
@@ -1,5 +1,10 @@
-export default defineNuxtPlugin(() => {
+export default defineNuxtPlugin(async () => {
   if (!("serviceWorker" in navigator)) return;
+
   if (useRuntimeConfig().public.isProd)
     navigator.serviceWorker.register("/sw.js");
+  else {
+    const registrations = await navigator.serviceWorker.getRegistrations();
+    registrations.forEach(x => x.unregister());
+  }
 });
