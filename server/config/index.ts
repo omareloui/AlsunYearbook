23f2e93c4e804a3357bf7639a6e88c82738c426c
Omar Hussein <omarelwy@hotmail.com>
2022-06-11 07:17:03 +0200

    🔧 Change the expiration for access token to 1m

--- server/config/index.ts
+++ server/config/index.ts
@@ -6,14 +6,13 @@ export const config = {
   dbLink: process.env.DB_URI,
 
   tokens: {
-    jwt: { secret: process.env.JWT_SECRET as string, expiration: "3m" },
+    jwt: { secret: process.env.JWT_SECRET as string, expiration: "1m" },
     refresh: {
       secret: process.env.REFRESH_TOKEN_SECRET as string,
       expiration: "30d",
     },
   },
 
-  // TODO: move this to run time config
   cloudinary: {
     name: process.env.CLOUDINARY_NAME,
     secret: process.env.CLOUDINARY_SECRET,
