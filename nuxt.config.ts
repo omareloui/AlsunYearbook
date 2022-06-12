78b6d367fd13c791b45d91d2ac139560b7fb9d06
Omar Hussein <omarelwy@hotmail.com>
2022-06-13 00:53:51 +0200

    🍱 Update the version link for the heading image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
           "A yearbook for University of Ain Shams' Alsun's class of 2017/2021.",
       },
 
-      { property: "og:image", content: "/images/heading.png" },
+      { property: "og:image", content: "/images/heading.png?v=2" },
       { property: "og:image:type", content: "image/png" },
       { property: "og:image:width", content: "1280" },
       { property: "og:image:height", content: "640" },
@@ -60,7 +60,7 @@ export default defineNuxtConfig({
       },
       {
         name: "twitter:image",
-        content: "/images/heading.png",
+        content: "/images/heading.png?v=2",
       },
     ],
 
